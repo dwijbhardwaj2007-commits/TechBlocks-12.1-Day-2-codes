c4eedbca380cc041e32a1b4df3858e9a4dadd088
Dwij Bhardwaj <dwijbhardwaj2007@gmail.com>
2026-09-20 12:12:18 +0530

    Add Opening_webcam.cpp file

--- Opening_webcam.cpp
+++ Opening_webcam.cpp
@@ -20,5 +20,6 @@ while True:
 cap.release()
 cv2.destroyAllWindows()
 
-  // In terminal in VS Code, type python webcam.py to run the program on windows
-  // Type python3 webcam.py to run the code on mac
+  // In terminal in VS Code, type python webcam.py to run the program on windows 
+ // Type python3 webcam.py to run the code on mac
+// Save the file with the filename "webcam.py"
